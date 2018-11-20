4f5b2ecd5f4100d233dcb830e6851efb1383bccb
Milly Rodriguez <millz118@gmail.com>
2018-11-20 17:44:59 -0500

    initial commit!

---
+++ src/components/Weather.js
@@ -0,0 +1,32 @@
+import React from 'react';
+// Class extension not needed for stateless functional components, they do not contain state so no need to use class, can just define as a function. *****BUT since props are being used, you must pass it in as an argument***** Can also remove the explicit return since it's implied with the arrow function!
+
+const Weather = props => (
+    <div className="weather__info">
+        {
+            props.city && props.country && <p className="weather__key">Location:
+            <span className="weather__value"> {props.city},  {props.country}</span>
+            </p>
+        }
+        {
+            props.city && props.country && <p className="weather__key"> Temperature:
+            <span className="weather__value"> {props.temperature}</span>
+            </p>
+        }
+        {
+            props.city && props.country && <p className="weather__key">Humidity:
+            <span className="weather__value"> {props.humidity}</span>
+            </p>
+        }
+        {
+            props.city && props.country && <p className="weather__key">Description:
+            <span className="weather__value"> {props.description}</span>
+            </p>
+        }
+        {
+            props.error && <p className="weather__error"> {props.error}</p>
+        }
+    </div>
+)
+
+export default Weather;
